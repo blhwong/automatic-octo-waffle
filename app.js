eece4fe716d1f77e5c404b20ffe63adc82a4eb09
Brandon Wong <b.lh.wong@gmail.com>
2017-05-27 20:52:41 -0700

    getting calendar is working

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var passport = require('passport');
-var path = require('path');
-var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const path = require('path');
+const config = require('./config');
+const google = require('googleapis');
+const googleAuth = require('google-auth-library');
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const axios = require('axios');
 
 app.use(require('morgan')('dev'));
+
+let localStorage = {};
 
 passport.serializeUser(function(user, cb) {
   cb(null, user);
@@ -15,30 +21,81 @@ passport.deserializeUser(function(obj, cb) {
   cb(null, obj);
 });
 
-app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+app.use(require('express-session')({
+  secret: config.secret,
+  resave: true,
+  saveUninitialized: true
+}));
 
 app.use(passport.initialize());
 app.use(passport.session());
 
 passport.use(new GoogleStrategy({
-  clientID: '774516533381-qtrivh6al97vulcsbdqsvfi9u8i610gt.apps.googleusercontent.com',
-  clientSecret: 'xfjWd-8GlaUgiWLyMTBbUz74',
+  clientID: config.clientID,
+  clientSecret: config.clientSecret,
   callbackURL: 'http://localhost:3007/auth/google/callback',
   scope: ['openid', 'email', 'https://www.googleapis.com/auth/calendar']
 },
   function(accessToken, refreshToken, profile, done) {
     process.nextTick(function() {
+      localStorage.accessToken = accessToken;
+      localStorage.refreshToken = refreshToken;
+      // console.log('profile', profile);
       return done(null, profile);
     });
   }
 ));
 
+checkAuthentication = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    //if user is loged in, req.isAuthenticated() will return true
+    next();
+  } else {
+    res.redirect('/auth/google');
+  }
+};
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.get('/calendar-events', (req, res) => {
-  res.send('calendar-events');
+app.get('/calendar-events', checkAuthentication, (req, res) => {
+  // res.send('calendar-events');
+  var auth = new googleAuth();
+  var oauth2Client = new auth.OAuth2(config.clientID, config.clientSecret, 'http://localhost:3007/auth/google/callback');
+  oauth2Client.credentials = {
+    access_token: localStorage.accessToken,
+    refresh_token: localStorage.refreshToken
+  };
+  console.log(oauth2Client);
+  var calendar = google.calendar('v3');
+  calendar.events.list({
+    auth: oauth2Client,
+    calendarId: 'b.lh.wong@gmail.com'
+    // timeMin: (new Date()).toISOString(),
+    // maxResults: 10,
+    // singleEvents: true,
+    // orderBy: 'startTime'
+  }, function(err, response) {
+    if (err) {
+      console.log('The API returned an error: ' + err);
+      // return;
+      // res.sendStatus(400);
+    }
+    console.log('response', response);
+    res.send(response);
+    // var events = response.items;
+    // if (events.length == 0) {
+    //   console.log('No upcoming events found.');
+    // } else {
+    //   console.log('Upcoming 10 events:');
+    //   for (var i = 0; i < events.length; i++) {
+    //     var event = events[i];
+    //     var start = event.start.dateTime || event.start.date;
+    //     console.log('%s - %s', start, event.summary);
+    //   }
+    // }
+  });
 });
 
 app.get('/auth/google',
@@ -46,18 +103,18 @@ app.get('/auth/google',
 
 app.get( '/auth/google/callback',
     passport.authenticate( 'google', {
-      failureRedirect: '/auth/google/failure'
+      failureRedirect: '/auth/google'
     }), (req, res) => {
-      res.redirect('/auth/google/success');
+      res.redirect('/calendar-events');
     });
 
-app.get('/auth/google/success', (req, res) => {
-  res.send('success login in');
-});
+// app.get('/auth/google/success', (req, res) => {
+//   res.send('success login in');
+// });
 
-app.get('/auth/google/failure', (req, res) => {
-  res.send('failure log in');
-});
+// app.get('/auth/google/failure', (req, res) => {
+//   res.send('failure log in');
+// });
 
 app.get('*', (req, res) => {
   res.redirect('/');
